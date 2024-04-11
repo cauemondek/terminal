ce79830b6ee1fc94eb1f91113e4e24ac41c4cf74
CaueMondek <cauemondekk@gmail.com>
2024-04-11 18:27:09 -0300

    :tada: init: First commit, some files pushed

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿const output = $('.output');
+
+function addInOutput() {
+    const newOutput = $('<div></div>').addClass('input-container');
+    const arrow = $('<h1>></h1>');
+    const textTyped = $('<p>'+ $('#inputEnter').val() + '</p>');
+
+    newOutput.appendTo(output);
+    arrow.appendTo(newOutput);
+    textTyped.appendTo(newOutput);
+}
+
+
+
+

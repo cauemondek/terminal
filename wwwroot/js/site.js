f81bdbe5eaff4c39b910cdc9f3026800a8d4b933
CaueMondek <cauemondekk@gmail.com>
2024-04-15 02:08:47 -0300

    :sparkles: feat: Boot system, fetch one API

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,119 @@
 ﻿const output = $('.output');
+let input = $('#inputEnter');
 
-function addInOutput() {
-    const newOutput = $('<div></div>').addClass('input-container');
-    const arrow = $('<h1>></h1>');
-    const textTyped = $('<p>'+ $('#inputEnter').val() + '</p>');
+$('#startSystem').on('click', () => {
+  $('#startSystemSound').prop('volume', 0.45);
+  $('#startSystemSound').trigger('play');
 
-    newOutput.appendTo(output);
-    arrow.appendTo(newOutput);
-    textTyped.appendTo(newOutput);
+  bootSystemA();
+});
+
+$('#startSystemSound').on('ended', () => {
+  $('#startTerminalSound').prop('volume', 0.8);
+  $('#startTerminalSound').trigger('play');
+
+  $('.bootSystemB').hide();
+
+  setTimeout(() => {
+    $('#startSystem').remove();
+    $('.bootSystemA').remove();
+    $('.bootSystemB').remove();
+
+    $('body').css('padding', '200px');
+    $('body').css('overflow-y', 'scroll');
+
+    $('.welcome').show();
+    $('.output').show();
+    $('.osIcon').show();
+    $('.input-container').css('display', 'flex');
+  }, 500);
+});
+
+function addInOutput(command) {
+  const newOutput = $('<div></div>').addClass('user-send-container');
+  const arrow = $('<h1>></h1>');
+  const textTyped = $('<p>' + $('#inputEnter').val() + '</p>');
+
+  newOutput.appendTo(output);
+  arrow.appendTo(newOutput);
+  textTyped.appendTo(newOutput);
+
+  $('<p>' + command + '</p>')
+    .addClass('console-message')
+    .appendTo(output);
 }
 
+$(document).on('keypress', e => {
+  if (e.keyCode == 13 && input.val() != '') {
+    addInOutput('Comando desconhecido, digite --help');
+    input.val('');
+    $(document).scrollTop(10000000000000);
+  }
+});
 
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    $('#ipsystem').text(data.ip);
+  })
+  .catch(error => {
+    console.log('Error to catch IP System: ', error);
+    $('#ipsystem').text('FATAL ERROR');
+  });
 
+function bootSystemA() {
+  setTimeout(() => {
+    $('.bootSystemA').show();
+  }, 1500);
 
+  setTimeout(() => {
+    $('.systemBootA-A').show();
+  }, 3000);
+
+  setTimeout(() => {
+    $('.systemBootA-B').show();
+  }, 4200);
+
+  const textDetectingBoot = document.querySelectorAll('.detectingBoot');
+  textDetectingBoot.forEach((index, number) => {
+    setTimeout(() => {
+      $(textDetectingBoot[0]).removeClass('animationUnderScore');
+      textDetectingBoot[0].innerHTML = 'OK';
+    }, 5000);
+    setTimeout(() => {
+      $(textDetectingBoot[1]).removeClass('animationUnderScore');
+      textDetectingBoot[1].innerHTML = 'OK';
+    }, 5900);
+    setTimeout(() => {
+      $(textDetectingBoot[2]).removeClass('animationUnderScore');
+      textDetectingBoot[2].innerHTML = 'OK';
+    }, 6800);
+  });
+
+  setTimeout(() => {
+    $('.bootSystemA').hide();
+
+    bootSystemB();
+  }, 7500);
+}
+
+function bootSystemB() {
+  $('body').css('overflow-y', 'hidden');
+  $('.bootSystemB').show();
+
+  setTimeout(() => {
+    $('.systemBootB-B').show();
+    let i = 0;
+    const addDots = setInterval(() => {
+      i++;
+      $('.systemBootB-B').text($('.systemBootB-B').text() + '.');
+      if (i == 6) {
+        clearInterval(addDots);
+      }
+    }, 200);
+  }, 1500);
+
+  setTimeout(() => {
+    $('.systemBootB-C').css('display', 'flex');
+  }, 3500);
+}

e9bda0af6881da780e064d61e51294b018df6243
cauemondek <cauemondekk@gmail.com>
2024-05-07 01:12:32 -0300

    :heavy_plus_sign: build: Add new packages and some configs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
+using TerminaldotNET.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
